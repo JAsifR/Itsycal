2e264968822cf2b5f2810f99c83a340a9b1ed632
Jahid <178466646+JAsifR@users.noreply.github.com>
2026-06-16 13:15:52 +0100

    Disable event tooltip UI

--- Itsycal/TooltipViewController.m
+++ Itsycal/TooltipViewController.m
@@ -2,24 +2,15 @@
 //  TooltipViewController.m
 //  Itsycal
 //
-//  Created by Sanjay Madan on 2/17/15.
-//  Copyright (c) 2015 mowglii.com. All rights reserved.
+//  Event tooltip UI disabled in this fork.
 //
 
 #import "TooltipViewController.h"
-#import "Themer.h"
 
 @implementation TooltipViewController
 
 - (BOOL)toolTipForDate:(MoDate)date
 {
-    self.tv.enableHover = NO;
-    self.tv.enclosingScrollView.hasVerticalScroller = NO; // in case user has System Prefs set to always show scroller
-    self.events = [self.tooltipDelegate eventsForDate:date];
-    if (self.events) {
-        [self reloadData];
-        return YES;
-    }
     return NO;
 }
 
